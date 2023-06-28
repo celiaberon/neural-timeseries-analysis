764dbd030aa0b0743f8a983052e24f575aa03d3c
Celia <celiaberon@gmail.com>
2023-06-28 13:30:20 -0400

    adding event alignment and event based avg plotting functions

---
+++ nta/events/align.py
@@ -0,0 +1,313 @@
+'''
+@author: celiaberon
+'''
+
+import pandas as pd
+import numpy as np
+from functools import partial
+
+def get_event_indices(timeseries: pd.DataFrame,
+                      *,
+                      aligned_event: str=None,
+                      align_to: str='onset',
+                      ) -> dict:
+
+    '''
+    Returns indices of specified events for each trial in a lookup table.
+
+    Args:
+        timeseries (pandas.DataFrame):
+            Timeseries data.
+        aligned_event (str):
+            Task event to align to (e.g. Select, Cue,Consumption, ENL...).
+        align_to (str):
+            Align to either 'onset' or 'offset' of event.
+
+    Returns:
+        event_idcs_by_trial (list):
+            Dict of {trial:indices} for all event (onset/offset) instances in
+            timeseries.
+    '''
+
+    # Use first or last instance of each event for alignment.
+    nth_val = 0 if align_to=='onset' else -1
+
+    # event alignment position per trial as index
+    event_idcs=(timeseries
+              .loc[timeseries[aligned_event]==1]
+              .groupby('nTrial', as_index=False)
+              .nth(nth_val)
+              .index
+              .values
+             )
+
+    # get trial ids to validate mapping into trial dataframe later
+    trial_ids = timeseries.loc[event_idcs].nTrial.values
+
+    # lookup table for event index by trial ID
+    event_idcs_by_trial = dict(zip(trial_ids, event_idcs))
+
+    return event_idcs_by_trial
+
+
+def align_single_trace(idx: int,
+                *,
+                ts: pd.DataFrame=None,
+                window_interp: list=None,
+                y_col: str=None,
+                ):
+
+    '''
+    Pull snippet of photometry timeseries with specified window around
+    event-based index. Exclude any windows missing data.
+
+    Args:
+        ts:
+            Timeseries of neural trace.
+        idx:
+            Alignment index in timeseries.
+        window_interp:
+            From align_photomery_epoch(), idx range to pull for plotting
+            aligned window.
+        y_col:
+            Column with neural data in ts.
+
+    Returns:
+        (np.array):
+            Timeseries of signal aligned by index
+            OR NaN if ANY missing values in aligned signal snippet.
+    '''
+
+    if idx is None:
+        return np.nan
+    try:
+        # Get list of indices from timeseries to pull aligned photometry trace
+        idcs = window_interp+idx
+        if ts.loc[idcs, 'session'].nunique() != 1:
+            return np.nan # don't allow crossing session boundaries
+        trace = ts.loc[idcs, y_col].values
+
+        return np.nan if np.any(np.isnan(trace)) else trace
+
+    except KeyError:
+        return np.nan # if missing any values full trace fails
+
+
+def align_photometry_to_event(trials: pd.DataFrame,
+                      ts_full: pd.DataFrame,
+                      *,
+                      channel: str=None,
+                      aligned_event: str=None,
+                      window: tuple=(1,3),
+                      sampling_freq: int=50,
+                      **kwargs
+                      ):
+
+    '''
+    Align photometry timeseries to behavior/task event and store snippets by
+    trial.
+
+    Args:
+        trials:
+            Trial dataframe with behavior/task variables.
+        ts_full:
+            Timeseries data with unbroken photometry stream used for
+            alignment.
+        channel:
+            'R' or 'L' specifying hemisphere of brain.
+        aligned_event:
+            Task event neural data will be aligned to.
+        window:
+            Photometry window as (seconds before, seconds after)
+            ALIGNED_EVENT.
+        sampling_freq:
+            Photometry sampling frequency in Hz.
+
+    Returns:
+        trials_:
+            Copy of trials with columns containing photometry snippet and
+            corresponding timestamps relative to aligned_event.
+    '''
+
+    # Ensure that our photometry timeseries has unbroken trial continuity.
+    if ts_full.session.nunique() > 1:
+        assert ts_full.groupby('session').nTrial.diff().max() == 1
+    else:
+        assert ts_full.nTrial.diff().max() == 1
+
+
+    trials_ = trials.copy()
+
+    # Get list of indices for timeseries by trial for each instance of event.
+    idx = get_event_indices(ts_full, aligned_event=aligned_event, **kwargs)
+    window_interp, timesteps = interpolate_window(window, sampling_freq)
+
+    # Need full unbroken timeseries of photometry data to properly address trial continuity.
+    align_trace_partial = partial(align_single_trace,
+                                  ts=ts_full,
+                                  window_interp=window_interp,
+                                  y_col=f'z_grn{channel}')
+
+    photo_column = f'{aligned_event}_{channel}_{window}'
+    times_column = f'{aligned_event}_times_{window}'
+
+    # Initialize as NaNs to handle trials without epoch.
+    trials_[photo_column] = np.nan
+    trials_[times_column] = np.nan
+
+    # Store snippet of photometry data alongside trial data.
+    trials_[photo_column]= (trials_['nTrial']
+                           .apply(lambda trial: align_trace_partial(idx=idx.get(trial, None))))
+
+    # Map times into full trial table only for trials with complete photometry snippets.
+    trials_with_data = trials_.dropna(subset=photo_column).copy()
+    trials_with_data[times_column] = [timesteps]*len(trials_with_data)
+    trials_[times_column] = (trials_['nTrial']
+                             .map(trials_with_data.set_index('nTrial')[times_column]))
+
+    return trials_
+
+
+def interpolate_window(window: tuple=(1,3),
+                                sampling_freq: int=50):
+
+    '''
+    Generates index interpolation for pre- and post-event window boundaries
+    (in seconds) based on sampling frequency
+
+    Args:
+        window (tuple):
+            Time in seconds before and after alignment event.
+        sampling_freq (int):
+            Sampling rate in Hz.
+
+    Returns:
+        window_interp (list):
+            List of sample number relative to alignment event in a single
+            window.
+        timesteps (list):
+            Timesteps in seconds corresponding to sample number.
+    '''
+
+    pre_window, post_window = window
+    pre_window *= sampling_freq # convert to number of sampling steps
+    pre_window = int(pre_window)
+    post_window *= sampling_freq # convert to number of sampling steps
+    post_window = int(post_window)
+
+    window_interp = np.arange(-pre_window, post_window) # num steps across window
+    timesteps = window_interp / sampling_freq # convert to units of seconds
+
+    return window_interp, timesteps
+
+
+def trials_licks_in_epoch(ts: pd.DataFrame, epoch: str):
+
+    '''
+    Get list of trials containing any licks within task epoch.
+
+    Args:
+        ts:
+            Timeseries data containing spout contacts and task epochs.
+        epoch:
+            Task state of interest (e.g. Cue, Consumption, Select).
+
+    Returns:
+        trials:
+            Array of trial IDs with licks in designated state.
+    '''
+
+    trials = ts.loc[(ts[epoch]==1) & (~np.isnan(ts.iSpout))].nTrial.unique()
+
+    return trials
+
+
+def trim_trials_without_licks(timeseries: pd.DataFrame, trials: pd.DataFrame):
+
+    '''
+    Filter data to include only trials that contain cue, selection lick, and
+    at least one lick in the consumption period.
+
+    Args:
+        timeseries:
+            Timeseries data containing spout contacts and task epochs.
+        trials:
+            Trial data to match with timeseries data.
+
+    Returns:
+        timeseries_trimmed:
+            Trimmed to include completed trials only.
+        trials_trimmed:
+            Trimmed to include completed trials only.
+    '''
+
+    trials_trimmed = trials.copy()
+    timeseries_trimmed = timeseries.copy()
+
+    # Get list of trials with completed task state for each epoch.
+    select_trials = trials_licks_in_epoch(timeseries_trimmed, 'Select')
+    consumption_trials = trials_licks_in_epoch(timeseries_trimmed, 'Consumption')
+    cue_trials = timeseries_trimmed.loc[timeseries_trimmed.Cue==1].nTrial.unique()
+
+    # Take the set of trials meeting criteria in all epochs.
+    included_trials = list(set(select_trials)
+                             .intersection(set((consumption_trials)))
+                             .intersection(set(cue_trials)))
+
+    # Subset each dataframe by set of trials.
+    timeseries_trimmed = timeseries_trimmed.query('nTrial.isin(@included_trials)').copy()
+    trials_trimmed = trials_trimmed.query('nTrial.isin(@included_trials)')
+
+    return timeseries_trimmed, trials_trimmed
+
+
+def get_lick_times(timeseries: pd.DataFrame,
+                   align_to: str='onset',
+                   **kwargs
+                   ) -> pd.DataFrame:
+
+    '''
+    Create dataframe containing event/lick times on trial based clock (in seconds)
+    with row for each trial. Includes Cue, penalties, Selection lick, ENL state, 
+    first Consumption lick.
+
+    Args:
+        timeseries:
+            Timeseries version of data containing analog states and spout
+            contacts.
+        align_to:
+            'onset' or 'offset' to designate storing time of first or last 
+            instance of event per trial.
+
+    Returns:
+        lick_times:
+            Dataframe containing event/lick times within each trial relative
+            to trial start (beginning of first ENL period).
+
+    '''
+
+    # Create a dataframe containing index as nTrial for mapping into.
+    lick_times = pd.DataFrame(index=timeseries.nTrial.unique())
+    lick_times.index.name = 'nTrial'
+
+    nth_val = 0 if align_to=='onset' else -1
+
+    # Get trial times for nth occurrence of event in each trial
+    for event in ['Cue', 'ENLP', 'Select', 'ENL']:
+        data_during_event = timeseries.loc[timeseries[event]==1].copy()
+        trials_with_event = data_during_event.nTrial.unique()
+        lick_times.loc[trials_with_event, event] = (data_during_event
+                                                    .groupby('nTrial')
+                                                    .nth(nth_val)['trial_clock'].values)
+
+    # As above for Consumption, for first lick only
+    # (requires extra conditioning on spout contact).
+    first_outcome_lick_idx = (timeseries
+                              .query('Consumption==1 & ~iSpout.isna()')
+                              .groupby('nTrial', as_index=False)
+                              .nth(0).index)
+    first_outcome_lick_trials = timeseries.loc[first_outcome_lick_idx].nTrial.unique()
+    first_outcome_lick_times = timeseries.loc[first_outcome_lick_idx].trial_clock.values
+    lick_times.loc[first_outcome_lick_trials,'Consumption'] = first_outcome_lick_times
+
+    return lick_times.reset_index()
---
+++ nta/visualization/avg_plots.py
@@ -0,0 +1,442 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 17 15:22:38 2022
+
+@author: celiaberon
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import matplotlib as mpl
+import configparser
+from functools import partial
+from nta.events.align import get_lick_times
+
+
+def load_config_variables(section: str='color_palette') -> dict:
+
+    '''
+    Create dictionary containing parameter values that will be repeated
+    across notebooks
+    
+    Args:
+        section: 
+           Section name within configuration file.
+    
+    Returns:
+        config_variables:
+            Dictionary containing variables and assigned values from config
+            file.
+    '''
+
+    # For color palette configuration only
+    import matplotlib as mpl
+    cpal = mpl.cm.RdBu_r(np.linspace(0, 1, 8))
+    
+    config_file = configparser.ConfigParser()
+    config_file.read("plot_config.ini")
+
+    # Create dictionary with key:value for each config item
+    config_variables = {}
+    for key in config_file[section]:
+        config_variables[key] = eval(config_file[section][key])
+
+    return config_variables
+
+
+def set_new_axes(n_iters: list,
+                 *,
+                 behavior_hist: bool=True,
+                 figsize: tuple=None,
+                 **kwargs):
+
+    '''
+    Configure axes for event alignment and possibly behavior distributions.
+    Size figure and individual axes based on set axes dims and number of
+    subplots total. Arrange for including behavior distributions if specified.
+
+    Args:
+        n_iters (list):
+            [total number of subplot groups, current subplot group]
+        behavior_hist (bool):
+            Whether or not to plot behavior distributions.
+        figsize: 
+            (width, height) dimensions.
+
+    Returns:
+        fig (matplotlib.figure object)
+        axs (matplotlib.axes object)
+    '''
+
+    if figsize is None:
+        subplot_width = 4.0
+        subplot_height = 3.0 + behavior_hist
+    else:
+        subplot_width, subplot_height = figsize
+
+    nrows = int(np.ceil(n_iters[0]/3))
+    ncols = min(n_iters[0], 3)
+
+    if behavior_hist: # with behavior distributions, will be 2 plots per alignment
+        fig, axs = plt.subplots(nrows=2*nrows, ncols=ncols,
+                                figsize=(subplot_width*ncols, subplot_height*nrows),
+                                sharex=True,
+                                gridspec_kw={'height_ratios':(3,1)})    
+    else: # Otherwise single layer of lineplots for each alignment
+        fig, axs = plt.subplots(nrows=nrows, ncols=ncols,
+                                figsize=(subplot_width*ncols, subplot_height*nrows),
+                                sharey=False, sharex=True)
+        if n_iters[0]==1:
+            axs = [axs, None] # need consistent dims for handling in plotting func
+
+    return fig, axs
+
+
+def set_current_axes(axs, 
+                     *,
+                     n_iters: list=None,
+                     behavior_hist: bool=None,
+                     ):
+
+    '''
+    Select current subplot axes for lineplot and behavior distributions
+    '''
+
+    ncols = min(n_iters[0], 3)
+
+    if n_iters[0]>1:
+        ax1 = axs.flatten()[n_iters[1]]
+        ax2 = axs.flatten()[n_iters[1]+ncols] if behavior_hist else None
+        # n_iters[1] += 1 # mutable update globally
+    else:
+        ax1, ax2 = axs
+
+    return ax1, ax2
+
+
+def subsample_trial_types(trials: pd.DataFrame,
+                          column: str,
+                          target_size: int=None) -> pd.DataFrame:
+
+    '''
+    Subsample to target size for each trial type to compare effects of rare
+    trial types.
+
+    Args:
+        trials:
+            Trial-based dataframe.
+        column:
+            Behavior variable to subsample condiitons within.
+        target_size:
+            Number of repetitions to sample from each group within column.
+    
+    Returns:
+        subsampled_trials:
+            Dataframe as subset of trials containing 
+            N trials = target_size * number of unique conditions in column.
+    '''
+
+    trials_ = trials.copy()
+
+    # Default to minimum number of reps across conditions in column.
+    min_trial_type = trials_[column].value_counts().min()
+    if (target_size is None) or (min_trial_type < target_size):
+        target_size = min_trial_type
+
+    subsampled_trials = trials_.groupby(column).sample(n=target_size)
+    
+    return subsampled_trials
+
+
+def config_plot_cpal(*, cmap_colors=None, **kwargs):
+
+    '''Set color palette with flexible input'''
+
+    match cmap_colors:
+        case int(): # Sequential palette for numerical conditions
+            cpal = mpl.cm.RdBu(np.linspace(0, 1, cmap_colors))
+            cpal = mpl.colors.LinearSegmentedColormap.from_list('cpal', cpal)
+        case (str() | dict()): # Use provided palette if given explicitly
+            cpal = cmap_colors
+        case _:
+            cpal = 'deep'
+
+    return cpal
+
+
+def plot_trial_type_comparison(ts: pd.DataFrame,
+                               *,
+                               column: str=None,
+                               align_event: str=None,
+                               y_col: str=None,
+                               trial_units: bool=False,
+                               behavior_hist: bool=False,
+                               n_iters: list=None,
+                               axs=None,
+                               fig=None,
+                               error=('ci', 95),
+                               **kwargs):
+
+    
+    '''
+    Plot lineplots of neural data aligned to behavioral/task events. Option
+    to also plot alongside distribution of behavior events.
+    
+    Args:
+        ts:
+            Timeseries of neural data also containing trial type information.
+        column:
+            Column within timeseries to condition plot color palette on.
+        align_event:
+            Behavior/task event neural data is aligned to (0 on x-axis).
+        y_col:
+            Hemisphere (R/L) and/or color (grn/red) to use for y-values.
+        trial_units:
+            Whether to plot individual traces for each trial.
+        behavior_hist:
+            Whether to include plots for behavior timing distributions.
+        n_iters: 
+            [number of total plot iterations, current plot iteration]
+        axs: 
+            matplotlib.axes object to add plots into.
+        fig: 
+            matplolib.figure object.
+        error: 
+            (error type, significance level)
+        **kwargs:
+            ls_col:
+                Second column to condition plotting on (line style).
+            legend_set:
+                Whether or not to include legend in plot.
+            ...
+    
+    Returns:
+        fig:
+            New/updated figure object.
+        axs:
+            New/updated axes object.
+    '''
+
+    sns.set(style='ticks', font_scale=1.0, rc={'axes.labelsize':11, 
+                                               'axes.titlesize':11,
+                                               'savefig.transparent':True,
+                                               'legend.title_fontsize': 11,
+                                               'legend.fontsize': 10})
+    
+    if n_iters is None:
+        n_iters = [1,0]
+
+    # set up plot aesthetics and designate current ax1: lineplot, ax2: behavior.
+    cpal = config_plot_cpal(**kwargs)
+    if n_iters[1]==0:
+        fig, axs = set_new_axes(n_iters=n_iters,
+                                behavior_hist=behavior_hist,
+                                **kwargs)
+    ax1, ax2 = set_current_axes(axs,
+                                n_iters=n_iters,
+                                behavior_hist=behavior_hist)
+
+    # Core plotting function regardless of individual trial traces or group mean.
+    window = kwargs.get('window', (1,2))
+    lineplot_core = partial(sns.lineplot, 
+                            x=f'{align_event}_times_{window}',
+                            y=y_col,
+                            hue=column,
+                            data=ts,
+                            ax=ax1,
+                            palette=cpal)
+    
+    # Actual lineplot for event-aligned neural data.
+    if trial_units:
+        ax1 = lineplot_core(label=None,
+                           units='nTrial',
+                           estimator=None,
+                           alpha=0.9)
+    else:
+        ax1 = lineplot_core(style=kwargs.get('ls_col', None),
+                            n_boot=kwargs.get('n_boot', 1000),
+                            errorbar=error,
+                            err_kws={'alpha':0.3},
+                            estimator=kwargs.get('estimator', 'mean'))
+
+    ax1 = config_plot(ax1, align_event, column, **kwargs)
+
+    # Plot distribution of behavioral/task events relative to alignment event.
+    if behavior_hist:
+        ax2 = behavior_event_distributions(ts, align_event, ax2,
+                                           column=column, **kwargs)
+
+    plt.tight_layout()
+    return fig, axs
+
+
+def plotting_wrapper(trials: pd.DataFrame,
+                     alignment_states: list=None,
+                     channel: str=None,
+                     **kwargs):
+
+    '''
+    Plot panel of photometry figures.
+
+    Args:
+        trials:
+            Trial-based data containing aligned photometry snippets and
+            timestamps.
+        alignment_states:
+            List of events to plot aligned neural data to.
+        channel:
+            L or R hemisphere photometry channel.
+        **kwargs: 
+            See plot_trial_type_comparison().
+    
+    Returns:
+        fig:
+            Figure object containing created and filled plot.
+        axs: 
+            Axes object containing created and filled plot.
+    '''
+
+    axs=None
+    fig=None
+    window = kwargs.get('window', (1,2))
+
+    # Default to plotting 3 main trial events.
+    if alignment_states is None:
+        alignment_states = ['Cue', 'Select', 'Consumption']
+
+    # Iteratively fill subplots with each event-alignd photometry trace.
+    for plot_iter, event in enumerate(alignment_states, start=1):
+        n_iters=[len(alignment_states),plot_iter-1]
+        photometry_column = f'{event}_{channel}_{window}'
+        exploded_trials = (trials.copy()
+                        .dropna(subset=photometry_column)
+                        .explode(column=[photometry_column, 
+                                        f'{event}_times_{window}'])
+                        )
+        fig, axs = plot_trial_type_comparison(exploded_trials,
+                                            align_event=event,
+                                            y_col=photometry_column,
+                                            n_iters=n_iters,
+                                            legend_set=plot_iter>=n_iters[0],
+                                            fig=fig,
+                                            axs=axs,
+                                            **kwargs)
+
+    return fig, axs
+
+
+def config_plot(ax, 
+                align_event: str, 
+                column: str,
+                legend_set: bool=False, 
+                ylim: tuple=(-2,3),
+                ls_col=False,
+                window: tuple=(1,3),
+                **kwargs):
+
+    '''
+    Configure plot aesthetics such as labeling 0 positions on axes
+    and setting limits/legends/tick labels consistently.
+    '''
+
+    ax.axvline(x=0, color='k', ls='-', lw=0.8, alpha=1.0, zorder=0, 
+               label=None)
+    ax.axhline(y=0, color='k', ls='-', lw=0.8, alpha=1.0, zorder=0)
+    ax.set(xlabel='Time (s)', ylabel='z-score', ylim=ylim)
+    ax.text(x=-0.05, y=ylim[1] + 0.1*sum(ylim), s=align_event)
+    ticks = ax.get_xticks()
+    ax.set_xticks([int(tick) for tick in ticks if tick.is_integer()])
+    ax.set(xlim=(-window[0],window[1]))
+
+    if not legend_set:
+        ax.legend().set_visible(False)
+    else:
+        ax.legend(bbox_to_anchor=(1, 1), loc='upper left', frameon=False,
+                title='' if ls_col else column)
+    sns.despine()
+
+    return ax
+
+
+def label_legend_unique_handles(ax, **kwargs):
+
+    handles, labels = ax.get_legend_handles_labels()
+    legend_reduced = dict(zip(labels, handles))
+    ax.legend(legend_reduced.values(), legend_reduced.keys(),
+                bbox_to_anchor=(1,1), frameon=False, **kwargs)
+    return ax
+
+
+def behavior_event_distributions(ts,
+                                 align_event,
+                                 ax,
+                                 *,
+                                 graded_cue: bool=False,
+                                 legend_set: bool=False,
+                                 **kwargs):
+
+    lick_times = get_lick_times(**kwargs)
+
+    # Default approach, calculate relative timing of 2 distributions to aligned event
+    state_colors={'Cue':sns.color_palette('colorblind')[3], 
+                  'Select':'darkgray', 
+                  'Consumption':'k'}
+    plotting_kwargs = {'color': True}
+    
+    if 'ENL' in align_event:
+        ts_ = ts.copy().groupby('nTrial')['T_ENL'].nth(0)
+        lick_times['T_ENL'] = lick_times['nTrial'].map(ts_)
+        plotting_kwargs = {'palette': sns.color_palette('Greys', n_colors=6)[1:],
+                           'hue':'T_ENL'}
+        
+    elif 'fake_event' in align_event: # For null comparisons
+        fake_events = ts.query('fake_event==1')
+        fake_event_trials = fake_events.nTrial.unique()
+        fake_event_times = fake_events.groupby('nTrial').nth(0)['trial_clock'].values
+        lick_times['fake_event'] = np.nan
+        lick_times.query('nTrial.isin(@fake_event_trials)')['fake_event'] = fake_event_times
+        
+    for event in ['Cue', 'Select', 'Consumption']:
+
+        if graded_cue:
+            raise(NotImplementedError)
+            # plotting_kwargs = {'hue': column, 'palette': kwargs.get('cpal')}
+            # ts_ = ts.copy().groupby('nTrial')[column].nth(0)
+            # lick_times[column] = lick_times['nTrial'].map(ts_)
+        elif plotting_kwargs.get('color', None) is not None:
+            plotting_kwargs['color'] = state_colors[event]
+
+        if event==align_event:
+            ax.axvline(x=0, color=state_colors[event])
+        else:
+            lick_times[f'{event} time'] = lick_times[event] - lick_times[align_event]
+            sns.histplot(data=lick_times, x=f'{event} time', ax=ax,
+                        binwidth=0.05, alpha=0.7,
+                        label=event, stat='probability',
+                        element='step', linewidth=0, edgecolor=None,
+                        **plotting_kwargs)
+            
+    ax.set(ylabel='fraction\ntrials', xlabel='Time (s)', ylim=(0,1))
+            
+    if not legend_set:
+        ax.legend().set_visible(False)
+
+    else:
+        # if graded_cue:
+        #     legend_labels,legend_handles=ax.get_legend_handles_labels()
+        #     ax.legend(legend_labels[:: len(legend_labels) // 2],
+        #             legend_handles[:: len(legend_labels) // 2],
+        #             frameon=False, fontsize=12, markerscale=0.8)        
+        # else:
+        def custom_patch(key, color):
+            from matplotlib.patches import Patch
+            return Patch(facecolor=color, edgecolor=None, label=key, alpha=0.7)
+        legend_elements = [custom_patch(key, color) for key, color in state_colors.items()]
+        ax.legend(handles=legend_elements, bbox_to_anchor=(1,1.5), 
+                    loc='upper left', frameon=False, markerscale=0.5, 
+                    )
+
+    return ax
