43e717b766fcf36ce5cf4d532d87ca0d729a1dbc
Celia <celiaberon@gmail.com>
2023-09-25 17:20:50 -0400

    formatting and style edits

--- nta/features/behavior_features.py
+++ nta/features/behavior_features.py
@@ -7,11 +7,11 @@ import pandas as pd
 
 
 def shift_trial_feature(trials: pd.DataFrame,
-                       col_name: str,
-                       n_shift: int,
-                       shift_forward: bool=True,
-                       new_col_name: str=''):
-    
+                        col: str,
+                        n_shift: int,
+                        shift_forward: bool = True,
+                        new_col: str = ''):
+
     '''
     Shift trial feature forward or backward to relate past or future events to
     current trial, respectively.
@@ -19,14 +19,14 @@ def shift_trial_feature(trials: pd.DataFrame,
     Args:
         trials:
             Trial-based dataframe.
-        col_name:
+        col:
             Column to be shifted.
         n_shift:
             Number of trials to shift the column.
         shift_forward:
             When True, shift column forward. New column will represent history
             of current trial.
-        new_col_name:
+        new_col:
             Can provide custom name for new column.
 
     Returns:
@@ -36,15 +36,15 @@ def shift_trial_feature(trials: pd.DataFrame,
 
     trials_ = trials.copy()
 
-    if not new_col_name:
-        new_col_name = col_name.lower()
+    if not new_col:
+        new_col = col.lower()
 
     if shift_forward:
-        # Use (-) to indicate trial histories (N trials back from current trial).
-        trials_[f'-{n_shift}{new_col_name}'] = trials_[f'{col_name}'].shift(n_shift)
+        # Use (-) for trial histories (N trials back from current trial).
+        trials_[f'-{n_shift}{new_col}'] = trials_[col].shift(n_shift)
     else:
-        # Use (+) to indicate subsequent trials (N trials forward from current).
-        trials_[f'+{n_shift}{new_col_name}'] = trials_[f'{col_name}'].shift(-n_shift)
+        # Use (+) for subsequent trials (N trials forward from current).
+        trials_[f'+{n_shift}{new_col}'] = trials_[col].shift(-n_shift)
 
     return trials_
 
@@ -71,15 +71,16 @@ def encode_choice_reward_pairing(trials: pd.DataFrame):
     trials_ = trials.copy()
 
     # Mappings from (Choice, Reward) tuples to letter-symbol code.
-    trial_mappings = {(1.0, 1.0):'R',
-                      (1.0, 0.0):'r',
-                      (0.0, 1.0):'L',
-                      (0.0, 0.0):'l'}
+    trial_mappings = {(1.0, 1.0): 'R',
+                      (1.0, 0.0): 'r',
+                      (0.0, 1.0): 'L',
+                      (0.0, 0.0): 'l'}
 
-    # Look up mapping for each trial that has value for both reward and direction.
-    seq_clean = trials_.copy().dropna(subset=['Reward','direction'])
-    trials_.loc[seq_clean.index, 'k1'] = [trial_mappings[h] for h in 
-                                          zip(seq_clean.direction, seq_clean.Reward)]
+    # Look up mapping for each trial with data (not null) for both reward and
+    # direction.
+    twd_ = trials_.copy().dropna(subset=['Reward', 'direction'])
+    trials_.loc[twd_.index, 'k1'] = [trial_mappings[h] for h in
+                                     zip(twd_.direction, twd_.Reward)]
 
     return trials_
 
@@ -139,56 +140,66 @@ def convert_to_AB_sequence(trials: pd.DataFrame,
     trials_ = trials.copy()
 
     # Mappings based on first direction chosen in the history sequence.
-    mappings_ref_L = {'L':'A', 'l':'a', 'R':'B', 'r':'b'}
-    mappings_ref_R = {'R':'A', 'r':'a', 'L':'B', 'l':'b'}
-    mappings_LUT = {'L':mappings_ref_L, 'R':mappings_ref_R}
+    mappings_ref_L = {'L': 'A', 'l': 'a', 'R': 'B', 'r': 'b'}
+    mappings_ref_R = {'R': 'A', 'r': 'a', 'L': 'B', 'l': 'b'}
+    mappings_LUT = {'L': mappings_ref_L, 'R': mappings_ref_R}
 
     column = f'RL_seq{sequence_length}'
 
     for i, row in trials_.iterrows():
 
-        if pd.isna(row[column]): continue
+        if pd.isna(row[column]):
+            continue
 
         reference_direction = row[f'k{sequence_length}'].upper()
         mappings = mappings_LUT[reference_direction]
-        trials_.loc[i, f'seq{sequence_length}'] = ''.join([mappings.get(s) for s in row[column]])
+        sequences = ''.join([mappings.get(s) for s in row[column]])
+        trials_.loc[i, f'seq{sequence_length}'] = sequences
 
     return trials_
 
 
 def get_reward_seq(trials: pd.DataFrame) -> pd.DataFrame:
 
-    '''Count sequential rewards and losses, restarting with alternative outcome.
-    
+    '''
+    Count sequential rewards and losses, restarting with alternative outcome.
+
     Args:
         trials (pandas.DataFrame):
             Trial-based dataframe.
-    
+
     Returns:
         trials_ (pandas.DataFrame):
             Copy of trials with new columns for reward and loss sequences.
     '''
-    
+
     trials_ = trials.copy()
     cumulative_rewards = trials_.Reward.cumsum()
-    cumulative_rewards_reset = cumulative_rewards.where(trials_.Reward==0).ffill().fillna(0).astype(int)
-    trials_['reward_seq'] = cumulative_rewards - cumulative_rewards_reset
+    cumulative_rewards_reset = (cumulative_rewards
+                                .where(trials_.Reward == 0)
+                                .ffill().fillna(0).astype(int))
+    trials_['rew_seq'] = cumulative_rewards - cumulative_rewards_reset
 
-    cumulative_losses = (1-trials_.Reward).cumsum()
-    cumulative_losses_reset = cumulative_losses.where(trials_.Reward>0).ffill().fillna(0).astype(int) 
+    cumulative_losses = (1 - trials_.Reward).cumsum()
+    cumulative_losses_reset = (cumulative_losses
+                               .where(trials_.Reward > 0)
+                               .ffill().fillna(0).astype(int))
     trials_['loss_seq'] = cumulative_losses - cumulative_losses_reset
-    
-    first_trials_to_clear = trials_.groupby('Session')['nTrial'].nth(slice(0,3)).values
-    trials_.loc[trials_.nTrial.isin(first_trials_to_clear), ['reward_seq', 'loss_seq']] = np.nan
+
+    # If sequence incomplete (doesn't have fully defined history), use NaNs.
+    inc_seq = (trials_
+               .groupby('Session')['nTrial']
+               .nth(slice(0, 3)).values)
+    trials_.loc[trials_.nTrial.isin(inc_seq), ['rew_seq', 'loss_seq']] = np.nan
 
     # Create single column with posiive values for reward, negative for loss.
-    trials_['outcome_seq'] = trials_['reward_seq'] - trials_['loss_seq'] 
+    trials_['outcome_seq'] = trials_['rew_seq'] - trials_['loss_seq']
 
     return trials_
 
 
 def add_timeseries_clock(timeseries: pd.DataFrame,
-                         fs: int=50):
+                         fs: int = 50):
 
     '''
     Estimate running clock across a session based on the known sampling rate
@@ -204,15 +215,15 @@ def add_timeseries_clock(timeseries: pd.DataFrame,
         session_clock (pd.Series):
             Column of clock times (in seconds) for each row in the timeseries.
     '''
-    
+
     if timeseries.session.dropna().nunique() > 1:
         raise NotImplementedError
-    
-    timeseries_ = timeseries.copy()
-    timeseries_['session_clock'] = 1/fs # convert each timestep to seconds
-    timeseries_['session_clock'] = timeseries_['session_clock'].cumsum()
 
-    return timeseries_['session_clock']
+    ts_ = timeseries.copy()
+    ts_['session_clock'] = 1 / fs  # convert each timestep to seconds
+    ts_['session_clock'] = ts_['session_clock'].cumsum()
+
+    return ts_['session_clock']
 
 
 def count_consumption_licks(timeseries: pd.DataFrame,
@@ -233,12 +244,12 @@ def count_consumption_licks(timeseries: pd.DataFrame,
     '''
 
     trials_ = trials.copy()
-    
+
     nLicks = (timeseries
-             .loc[(timeseries.Consumption==1) & (~timeseries.iSpout.isnull())]
-             .groupby(['nTrial'])['iSpout']
-             .agg(len)
-             )
+              .query('Consumption == 1 & iSpout.isin([1, 2, 3])')
+              .groupby(['nTrial'])['iSpout']
+              .agg(len)
+              )
     trials_['nLicks'] = trials_['nTrial'].map(nLicks)
 
     return trials_['nLicks']
@@ -260,23 +271,22 @@ def label_lick_position(timeseries: pd.DataFrame):
             Column of lick position labels starting with Select=1.
     '''
 
-    timeseries_ = timeseries.copy()
+    ts_ = timeseries.copy()
 
-    timeseries_['iLick'] = np.nan
-    timeseries_['iLick'] = (timeseries_.loc[(~timeseries_.iSpout.isna()) 
-                             & ((timeseries_.Consumption==1) | (timeseries_.Select==1))]
-                          .groupby('nTrial')
-                          .cumcount())
-    
-    return timeseries_['iLick']
+    ts_['iLick'] = np.nan
+    ts_['iLick'] = (ts_.query('~iSpout.eq(0) & Consumption == 1 | Select == 1')
+                       .groupby('nTrial')
+                       .cumcount())
+
+    return ts_['iLick']
 
 
 def add_behavior_cols(trials: pd.DataFrame,
                       timeseries: pd.DataFrame,
-                      fs: int=50) -> tuple:
+                      fs: int = 50) -> tuple:
 
     '''
-    Add columns defining behavior features to trial data. 
+    Add columns defining behavior features to trial data.
 
     Args:
         trials:
@@ -300,40 +310,46 @@ def add_behavior_cols(trials: pd.DataFrame,
     '''
 
     trials_ = trials.copy()
-    timeseries_ = timeseries.copy()
+    ts_ = timeseries.copy()
 
-    assert trials_.Session.dropna().nunique()==1 # because of row shifting
+    assert trials_.Session.dropna().nunique() == 1  # because of row shifting
 
-    trials_ = get_reward_seq(trials_) # number cumulative rewarded and losses
-    trials_ = shift_trial_feature(trials, col_name='outcome_seq', n_shift=1,
-                                 shift_forward=True,
-                                 new_col_name='outcome_seq_history')
+    trials_['enlp_trial'] = trials_['n_ENL'] > 1
+    trials_ = get_reward_seq(trials_)  # number cumulative rewarded and losses
+    trials_ = shift_trial_feature(trials_, col='outcome_seq', n_shift=1,
+                                  shift_forward=True,
+                                  new_col='outcome_seq_history')
 
     # Create column with choice-outcome code to construct trial histories.
     trials_ = encode_choice_reward_pairing(trials_)
-    
+
     # Build up columns defining sequential history for each trial and
     # history length. NOTE: here increasing numbers denote advancing
     # back into history from k1=current trial.
-    history_lengths = range(2,4)
-    for history_length in range(1, max(history_lengths)):
-        trials_[f'k{history_length+1}'] = trials_['k1'].shift(history_length)
-    for history_length in history_lengths:
-        trials_ = build_history_sequence(trials_, history_length)
-        trials_ = convert_to_AB_sequence(trials_, sequence_length=history_length)
+    history_lengths = range(2, 4)
+    for h_length in range(1, max(history_lengths)):
+        trials_[f'k{h_length + 1}'] = trials_['k1'].shift(h_length)
+    for h_length in history_lengths:
+        trials_ = build_history_sequence(trials_, h_length)
+        trials_ = convert_to_AB_sequence(trials_, sequence_length=h_length)
 
     # Some additional columns that can be useful.
-    timeseries_['session_clock'] = add_timeseries_clock(timeseries_, fs=fs)    
-    trials_['nLicks'] = count_consumption_licks(timeseries_, trials_)
-    timeseries_['iLick'] = label_lick_position(timeseries)
-    
+    ts_['session_clock'] = add_timeseries_clock(ts_, fs=fs)
+    trials_['nLicks'] = count_consumption_licks(ts_, trials_)
+    ts_['iLick'] = label_lick_position(ts_)
+
     # Calculate interlick interval (independent of state lick occurs within).
-    timeseries_['ILI'] = timeseries_.loc[~timeseries_.iSpout.isna()].session_clock.diff()
+    ts_['ILI'] = ts_.query('~iSpout.isna()').session_clock.diff()
 
     # Forward and backward shifts that can be useful (need to shift up front).
-    for col in ['seq2', 'seq3', 'tSelection', 'direction', 'Reward', 'nLicks']:
-        trials_ = shift_trial_feature(trials_, col_name=col, n_shift=1, shift_forward=True)
-    for col in ['seq2', 'seq3', 'tSelection', 'direction', 'Switch']:
-        trials_ = shift_trial_feature(trials_, col_name=col, n_shift=1, shift_forward=False)
-    
-    return trials_, timeseries_
+    history_features = ['seq2', 'seq3', 'tSelection', 'direction', 'Reward',
+                        'nLicks']
+    future_features = ['seq2', 'seq3', 'tSelection', 'direction', 'Switch']
+    for feature in history_features:
+        trials_ = shift_trial_feature(trials_, col=feature, n_shift=1,
+                                      shift_forward=True)
+    for feature in future_features:
+        trials_ = shift_trial_feature(trials_, col=feature, n_shift=1,
+                                      shift_forward=False)
+
+    return trials_, ts_
