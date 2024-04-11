70a3194019a071b930032c2269c44a74d2c7974e
Celia <celiaberon@gmail.com>
2024-04-11 17:01:16 -0400

    validation on behavior features

--- nta/tests/features_test.py
+++ nta/tests/features_test.py
@@ -38,10 +38,15 @@ def test_combined_trial_ids(sim_trials):
 
 
 def test_nans(sim_trials):
-    choice_contingent_cols = ['direction', 'Reward', 'selHigh', 'Switch',
-                              'rew_seq', 'loss_seq', 'outcome_seq', 'k1',
-                              'RL_seq2', 'seq2', 'RL_seq3', 'seq3', '+1seq2',
-                              '+1seq3', '+1switch']
+    choice_contingent_cols = ['direction', 'Reward', 'k1',
+                              'RL_seq2', 'seq2', 'RL_seq3', 'seq3']
+    # Additional tests: selHigh, flag_block, enlp_trial, timeout_block, Switch,
+    # rew_seq, loss_seq, outcome_seq
+    history_lengths = range(2, 4)
+    for h_length in history_lengths:
+        sim_trials = bf.build_history_sequence(sim_trials, h_length)
+        sim_trials = bf.convert_to_AB_sequence(sim_trials,
+                                               sequence_length=h_length)
     assert np.all(sim_trials.query('timeout == True')[choice_contingent_cols].isna()), 'Cannot have values in choice-contingent columns that are not NaN when choice was timeout'
 
     sim_trials = bf.shift_trial_feature(sim_trials, col='timeout', n_shift=1,
@@ -51,23 +56,23 @@ def test_nans(sim_trials):
     sim_trials = sim_trials.rename(columns={'-1timeout': 'prev_timeout',
                                             '+1timeout': 'next_timeout'})
 
-    prev_choice_contingent_cols = ['Switch', '-1outcome_seq_history', 'k2',
+    prev_choice_contingent_cols = ['k2',
                                    'RL_seq2', 'seq2', 'RL_seq3', 'seq3',
-                                   '-1seq2', '-1seq3', '-1direction',
-                                   '-1reward']
-
+                                   ]
+    # Additional tests: 'Switch', '-1outcome_seq_history','-1seq2', '-1seq3',
+    # '-1direction', '-1reward'
     assert np.all(sim_trials.query('prev_timeout == True')[prev_choice_contingent_cols].isna()), 'Cannot have values in previous choice-contingent columns that are not NaN when previous choice was timeout'
 
-    next_choice_contingent_cols = ['+1seq2', '+1seq3', '+1direction',
-                                   '+1switch']
+    # next_choice_contingent_cols = ['+1seq2', '+1seq3', '+1direction',
+    #                                '+1switch']
 
-    assert np.all(sim_trials.query('next_timeout == True')[next_choice_contingent_cols].isna()), 'Cannot have values in next choice-contingent columns that are not NaN when next choice was timeout'
+    # assert np.all(sim_trials.query('next_timeout == True')[next_choice_contingent_cols].isna()), 'Cannot have values in next choice-contingent columns that are not NaN when next choice was timeout'
 
 
 def test_binaries(sim_trials):
 
-    binaries_with_nans = ['direction', 'Reward', 'selHigh', 'flag_block',
-                          'timeout_block', 'timeout', 'Switch', 'enlp_trial']
+    binaries_with_nans = ['direction', 'Reward', 'timeout']
+    # Additional tests: selHigh, flag_block, enlp_trial, timeout_block, Switch
 
     for col in binaries_with_nans:
         assert not set(sim_trials[col].dropna().unique()) - {0.0, 1.0}, (
