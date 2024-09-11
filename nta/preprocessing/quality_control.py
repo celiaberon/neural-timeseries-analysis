65a6b8cb22903a970af794ffc410e8d93df12f47
Celia <celiaberon@gmail.com>
2024-09-11 14:45:01 -0400

    added 100-0 condition

--- nta/preprocessing/quality_control.py
+++ nta/preprocessing/quality_control.py
@@ -149,7 +149,8 @@ def QC_session_performance(trials: pd.DataFrame,
                              '9010': 0.7,
                              8020: 0.6,
                              '8020': 0.6,
-                             '100-1': 0.7}
+                             '100-1': 0.7,
+                             '100-0': 0.6}
     TARGET_AVG = condition_perf_thresh.get(trials.Condition.unique()[0])
     SIDE_BIAS = 0.1
     SPOUT_BIAS = 0.15
