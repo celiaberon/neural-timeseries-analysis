45947825ac6c0592559cfb085d719f6b57bf8514
Celia <celiaberon@gmail.com>
2024-11-25 15:38:14 -0500

    simulating data with multiple sampling rates

--- nta/data/simulations.py
+++ nta/data/simulations.py
@@ -21,69 +21,69 @@ class HeadfixedTask:
         self.ts = pd.DataFrame()
         self.trials = pd.DataFrame()
 
-    def make_trial(self):
+    # def make_trial(self):
 
-        '''
-        Create timeseries for trial of major task events based on  task
-        specifications.
-        '''
+    #     '''
+    #     Create timeseries for trial of major task events based on  task
+    #     specifications.
+    #     '''
 
-        tstep = self.timestep  # sec = index_units * tstep
+    #     tstep = self.timestep  # sec = index_units * tstep
 
-        # Draw current ENL duration as index for 50 Hz samples.
-        penalty = np.random.choice([0, 1], p=[0.8, 0.2])
-        enl = int(npr.choice(self.enl_dist) // tstep)
-        penalty_idx = int(np.random.random() * enl) if penalty else 0
-        cue_dur = self.cue_duration / tstep
+    #     # Draw current ENL duration as index for 50 Hz samples.
+    #     penalty = np.random.choice([0, 1], p=[0.8, 0.2])
+    #     enl = int(npr.choice(self.enl_dist) // tstep)
+    #     penalty_idx = int(np.random.random() * enl) if penalty else 0
+    #     cue_dur = self.cue_duration / tstep
 
-        # Draw current selection time as index for 50 Hz samples.
-        sel_time = int(npr.normal(loc=0.3, scale=0.1) // tstep)
-        timeout = (sel_time * tstep) > self.max_selection_time
+    #     # Draw current selection time as index for 50 Hz samples.
+    #     sel_time = int(npr.normal(loc=0.3, scale=0.1) // tstep)
+    #     timeout = (sel_time * tstep) > self.max_selection_time
 
-        # Create first consumption lick as noisy 135 Hz oscillator from
-        # selection.
-        second_lick_delay = npr.normal(loc=.135, scale=.02)
-        second_lick_delay = np.clip(second_lick_delay, 0, 3) // tstep
-        cons_lick = sel_time + int(second_lick_delay)
+    #     # Create first consumption lick as noisy 135 Hz oscillator from
+    #     # selection.
+    #     second_lick_delay = npr.normal(loc=.135, scale=.02)
+    #     second_lick_delay = np.clip(second_lick_delay, 0, 3) // tstep
+    #     cons_lick = sel_time + int(second_lick_delay)
 
-        # Number of samples comprising the full trial as a timeseries.
-        n_samples = int(sum((penalty_idx,
-                             enl,
-                             cue_dur,
-                             sel_time,
-                             (self.consumption_period / tstep))))
+    #     # Number of samples comprising the full trial as a timeseries.
+    #     n_samples = int(sum((penalty_idx,
+    #                          enl,
+    #                          cue_dur,
+    #                          sel_time,
+    #                          (self.consumption_period / tstep))))
 
-        # Make dataframe for current trial as a timeseries of evolving states.
-        states = ['Cue', 'Select', 'Consumption']
-        curr_trial_ts = {state: np.zeros(n_samples) for state in states}
-        curr_trial_ts = pd.DataFrame(curr_trial_ts)
-        curr_trial_ts['step'] = tstep
-        curr_trial_ts.loc[enl + penalty_idx, 'Cue'] = 1
-        if penalty:
-            curr_trial_ts.loc[penalty_idx, 'ENLP'] = 1
+    #     # Make dataframe for current trial as a timeseries of evolving states.
+    #     states = ['Cue', 'Select', 'Consumption']
+    #     curr_trial_ts = {state: np.zeros(n_samples) for state in states}
+    #     curr_trial_ts = pd.DataFrame(curr_trial_ts)
+    #     curr_trial_ts['step'] = tstep
+    #     curr_trial_ts.loc[enl + penalty_idx, 'Cue'] = 1
+    #     if penalty:
+    #         curr_trial_ts.loc[penalty_idx, 'ENLP'] = 1
 
-        # Trial can only contain selection and consumption events if selection
-        # occurs before max selection time.
-        if not timeout:
-            curr_trial_ts.loc[enl + sel_time + cue_dur, 'Select'] = 1
-            curr_trial_ts.loc[enl + cons_lick + cue_dur, 'Consumption'] = 1
-        curr_trial_ts['nTrial'] = self.nTrial
+    #     # Trial can only contain selection and consumption events if selection
+    #     # occurs before max selection time.
+    #     if not timeout:
+    #         curr_trial_ts.loc[enl + sel_time + cue_dur, 'Select'] = 1
+    #         curr_trial_ts.loc[enl + cons_lick + cue_dur, 'Consumption'] = 1
+    #     curr_trial_ts['nTrial'] = self.nTrial
 
-        # Make dataframe for current trial as a timeseries of evolving states.
-        current_trial = {'nTrial': self.nTrial,
-                         'Reward': self.assign_reward(),
-                         't_cue_to_sel': (sel_time + cue_dur) * tstep,
-                         't_sel_to_cons': second_lick_delay * tstep,
-                         't_cue_to_cons': (cons_lick + cue_dur) * tstep,
-                         't_sel_pre_cons': -second_lick_delay * tstep,
-                         't_cue_pre_cons': -(cons_lick + cue_dur) * tstep,
-                         't_cue_pre_sel': -(sel_time + cue_dur) * tstep,
-                         'enlp_trial': penalty}
-        current_trial = pd.DataFrame(current_trial, index=[0])
+    #     # Make dataframe for current trial as a timeseries of evolving states.
+    #     current_trial = {'nTrial': self.nTrial,
+    #                      'Reward': self.assign_reward(),
+    #                      't_cue_to_sel': (sel_time + cue_dur) * tstep,
+    #                      't_sel_to_cons': second_lick_delay * tstep,
+    #                      't_cue_to_cons': (cons_lick + cue_dur) * tstep,
+    #                      't_sel_pre_cons': -second_lick_delay * tstep,
+    #                      't_cue_pre_cons': -(cons_lick + cue_dur) * tstep,
+    #                      't_cue_pre_sel': -(sel_time + cue_dur) * tstep,
+    #                      'enlp_trial': penalty}
+    #     current_trial = pd.DataFrame(current_trial, index=[0])
 
-        self.nTrial += 1
+    #     self.nTrial += 1
 
-        return curr_trial_ts, current_trial
+    #     return curr_trial_ts, current_trial
 
     def assign_reward(self):
 
@@ -110,9 +110,104 @@ class HeadfixedTask:
         session = f'sim_session_{session_id}'
         self.ts_['Session'] = session
         self.trials_['Session'] = session
-        self.ts_['iSpout'] = self.ts_['Select'].copy()
-        self.ts_['iSpout'] *= (self.ts_.nTrial // 20) % 2
-        # self.trials['Session'] = 'sim_session'
+
+    def make_trial(self):
+
+        # Single penalties only, 20% chance of happening.
+        penalty = np.random.choice([0, 1], p=[0.8, 0.2])
+
+        # Draw current ENL duration as index for 50 Hz samples.
+        enl = npr.choice(self.enl_dist)
+
+        # Time of penalty based on ENL duration.
+        t_penalty = int(np.random.random() * enl) if penalty else 0
+
+        # Selection time, with mean of 300 ms.
+        sel_time = npr.normal(loc=0.3, scale=0.1)
+
+        # Timeout if greater than max permissble selection time.
+        timeout = sel_time > self.max_selection_time
+
+        # Create first consumption lick as noisy 135 Hz oscillator from
+        # selection.
+        second_lick_delay = npr.normal(loc=.135, scale=.02)
+        second_lick_delay = np.clip(second_lick_delay, 0, 3)
+        cons_lick = sel_time + second_lick_delay
+
+        # Make dataframe for current trial as a timeseries of evolving states.
+        current_trial = {
+            'nTrial': self.nTrial,
+            'Reward': self.assign_reward(),
+            't_cue_to_sel': sel_time + self.cue_duration,
+            't_sel_to_cons': second_lick_delay,
+            't_cue_to_cons': cons_lick + self.cue_duration,
+            't_sel_pre_cons': -second_lick_delay,
+            't_cue_pre_cons': -(cons_lick + self.cue_duration),
+            't_cue_pre_sel': -(sel_time + self.cue_duration),
+            'tSelection': sel_time,
+            'enlp_trial': penalty,
+            't_penalty': t_penalty,
+            'T_ENL': enl,
+            'timeout': timeout}
+        current_trial = pd.DataFrame(current_trial, index=[0])
+
+        # current_trial_events = pd.DataFrame(trial_events, index=[0])
+        self.nTrial += 1
+
+        return current_trial
+
+    def make_trial_timeseries(self, current_trial, tstep=None):
+
+        tstep = tstep or self.timestep  # sec = index_units * tstep
+
+        # ENL penalty occurs at t_penalty
+        penalty_idx = int(current_trial['t_penalty'].item() // tstep)
+
+        # ENL idx denotes end of ENL (onset of cue)
+        enl_idx = penalty_idx + int(current_trial['T_ENL'].item() // tstep)
+
+        # Cue duration is index at END of cue
+        cue_dur_idx = enl_idx + int(self.cue_duration / tstep)
+
+        # Selection lick delayed from end of cue.
+        sel_lick_idx = cue_dur_idx + int(current_trial['tSelection'].item() // tstep)
+
+        # Create first consumption lick as noisy 135 Hz oscillator from
+        # selection.
+        cons_lick_idx = sel_lick_idx + int(current_trial['t_sel_to_cons'].item() // tstep)
+
+        # Number of samples comprising the full trial as a timeseries.
+        n_samples = int(sum((sel_lick_idx,
+                            (self.consumption_period / tstep))))
+
+        # Make dataframe for current trial as a timeseries of evolving states.
+        states = ['Cue', 'Select', 'Consumption', 'ENL', 'ENLP', 'sync_pulse', 'stateConsumption']
+        curr_trial_ts = pd.DataFrame({state: np.zeros(n_samples) for state in states})
+
+        curr_trial_ts['nTrial'] = current_trial['nTrial'].item()
+        curr_trial_ts['step'] = tstep
+        curr_trial_ts['fs'] = 1 / tstep
+        curr_trial_ts.loc[0, 'sync_pulse'] = 1
+
+        if current_trial['enlp_trial'].item():
+            curr_trial_ts.loc[penalty_idx, 'ENLP'] = 1
+
+        curr_trial_ts.loc[penalty_idx:enl_idx, 'ENL'] = 1
+        curr_trial_ts.loc[enl_idx:cue_dur_idx, 'Cue'] = 1  # 'Cue onset
+
+        # Trial can only contain selection and consumption events if selection
+        # occurs before max selection time.
+        if not current_trial['timeout'].item():
+            curr_trial_ts.loc[sel_lick_idx, 'Select'] = 1
+            curr_trial_ts.loc[cons_lick_idx, 'Consumption'] = 1
+            curr_trial_ts.loc[cons_lick_idx:, 'stateConsumption'] = 1
+
+        curr_trial_ts['iSpout'] = curr_trial_ts['Select'].copy()
+        curr_trial_ts['iSpout'] *= (current_trial['nTrial'].item() // 20) % 2
+
+        curr_trial_ts['trial_clock'] = curr_trial_ts['step'].cumsum() - tstep
+
+        return curr_trial_ts
 
     def generate_session(self, total_trials=300, session_id=0):
 
@@ -123,16 +218,15 @@ class HeadfixedTask:
 
         session_trials = []
         session_timeseries = []
-        for trial in range(total_trials):
-            timeseries, trials = self.make_trial()
-            timeseries['trial_clock'] = self.timestep
-            timeseries['trial_clock'] = timeseries['trial_clock'].cumsum()
+        for _ in range(total_trials):
+            trial = self.make_trial()
+            timeseries = self.make_trial_timeseries(trial)
             session_timeseries.append(timeseries)
-            session_trials.append(trials)
+            session_trials.append(trial)
 
         session_timeseries = (pd.concat(session_timeseries)
                               .reset_index(drop=True))
-        session_timeseries['session_clock'] = session_timeseries.step.cumsum()
+        session_timeseries['session_clock'] = session_timeseries.step.cumsum() - self.timestep
 
         self.ts_ = session_timeseries
         self.trials_ = pd.concat(session_trials).reset_index(drop=True)
@@ -145,6 +239,27 @@ class HeadfixedTask:
                                   session_id=session)
             self.ts = pd.concat((self.ts, self.ts_)).reset_index(drop=True)
             self.trials = pd.concat((self.trials, self.trials_)).reset_index(drop=True)
+
+    def generate_timeseries_only(self, tstep=None):
+
+        print(f'generating timeseries with new sampling rate: {round(1 / tstep, 2)}')
+
+        multi_session_timeseries = []
+        for session_id, session_data in self.trials.groupby('Session'):
+            session_timeseries = []
+            for trial_id, trial in session_data.groupby('nTrial'):
+                timeseries = self.make_trial_timeseries(trial, tstep)
+                session_timeseries.append(timeseries)
+
+            session_timeseries = (pd.concat(session_timeseries)
+                                    .reset_index(drop=True))
+
+            # Keep on same overall clock (accounts for slight differences in
+            # number of samples resulting from different construction rates).
+            session_timeseries['session_clock'] = np.linspace(0, self.ts['session_clock'].iloc[-1], len(session_timeseries))
+            session_timeseries['Session'] = session_id
+            multi_session_timeseries.append(session_timeseries)
+        self.ts_resampled = pd.concat(multi_session_timeseries).reset_index(drop=True)
 
     def generate_noisy_events(self, mean_amp=1.5, noise=True):
 
@@ -199,18 +314,29 @@ class HeadfixedTask:
 
         # Create exponential Gaussian with assymmetric rise and fall kinetics.
         # Std chosen to roughly preserve amplitude of event.
-        gauss_filter = signal.windows.gaussian(M=10, std=0.43)
+        gauss_filter = signal.windows.gaussian(M=8, std=0.43)
         gauss_filter = np.clip(gauss_filter, 0, np.inf)
         self.gauss_filter = gauss_filter
         convolved_sig = signal.convolve(self.ts.amplitudes.values,
                                         gauss_filter)
         self.ts['amplitudes_gauss'] = convolved_sig[:len(self.ts)]
 
-        exp_filter = signal.windows.exponential(140, 0, tau=20, sym=False)
-        self.exp_filter = exp_filter
-        convolved_sig = signal.convolve(self.ts.amplitudes_gauss.values,
-                                        exp_filter)
-        self.ts['grnL'] = convolved_sig[:len(self.ts)]
+        exp_filter_pos = signal.windows.exponential(140, 0, tau=15, sym=False)
+        self.exp_filter_pos = exp_filter_pos
+        pos_signal = self.ts.amplitudes_gauss.values * (self.ts.amplitudes_gauss.values >= 0)
+        convolved_sig_pos = signal.convolve(pos_signal,
+                                            exp_filter_pos)
+
+        # Separate decay rate for negative levels.
+        exp_filter_neg = signal.windows.exponential(140, 0, tau=40,
+                                                    sym=False) / 2
+        self.exp_filter_neg = exp_filter_neg
+        neg_signal = self.ts.amplitudes_gauss.values * (self.ts.amplitudes_gauss.values < 0)
+        convolved_sig_neg = signal.convolve(neg_signal,
+                                            exp_filter_neg)
+
+        # Combine positive and negative components
+        self.ts['grnL'] = convolved_sig_pos[:len(self.ts)] + convolved_sig_neg[:len(self.ts)]
 
     def plot_event_to_kernel(self):
 
@@ -223,13 +349,25 @@ class HeadfixedTask:
         sim_sig = np.zeros(100)
         sim_sig[50] = 3
 
-        # Convolve impulse with gaussian then exponential kernels.
+        # Convolve impulse with gaussian then positive exponential kernel.
         gauss_sig = signal.convolve(sim_sig, self.gauss_filter)
-        exp_gauss_sig = signal.convolve(gauss_sig, self.exp_filter)
+        exp_gauss_sig_pos = signal.convolve(gauss_sig * (gauss_sig >= 0),
+                                            self.exp_filter_pos)
 
         fig, ax = plt.subplots(figsize=(3, 2))
         ax.plot(sim_sig, lw=1.5, label='simulated event')
-        ax.plot(exp_gauss_sig, lw=1.5, alpha=0.8, label='filtered event')
+        ax.plot(exp_gauss_sig_pos, lw=1.5, alpha=0.8, label='filtered event')
+
+        # Create single impulse event of magnitude = -3.
+        sim_sig = np.zeros(100)
+        sim_sig[50] = -3
+
+        # Convolve impulse with gaussian then negatvie exponential kernel.
+        gauss_sig = signal.convolve(sim_sig, self.gauss_filter)
+        exp_gauss_sig_neg = signal.convolve(gauss_sig * (gauss_sig < 0),
+                                            self.exp_filter_neg)
+        ax.plot(exp_gauss_sig_neg, lw=1.5, alpha=0.8, label='filtered event')
+
         ax.set(xlabel='n points', ylabel='event magnitude')
         plt.legend()
         sns.despine()
